61155b6ada5d3732d60d07270e67856305856b6c
Elizabeth <ElizabethSanchez@crbc.net>
2015-05-13 09:02:59 -0400

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Liz Sanchez on 5/13/15.
+//  Copyright (c) 2015 Liz Sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Liz Sanchez on 5/13/15.
+//  Copyright (c) 2015 Liz Sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
